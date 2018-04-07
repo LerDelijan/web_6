18f8ce81fbc724c4a55eef8f62bfc7027a092e27
Ler Delijan <lerdelijan@gmail.com>
2018-04-07 18:15:50 +0300

    init

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+    { name: 'Excalibur', price: 520.30 },
+    { name: 'Shining Armor', price: 870.05 },
+    { name: 'Helmet of the Arena Champion', price: 290.70 },
+    { name: 'Fuzzies Potion', price: 30 },
+    { name: 'Healing Potion', price: 74.2 },
+    { name: 'Potion of Pure Reason', price: 89.7 },
+    { name: 'Scroll of Resurrection', price: 132.5 }
+])
